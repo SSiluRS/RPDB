5527c2b3686511182bb42649708dbf73498a74f7
SSiluRS <Endercresman397@mail.ru>
2022-12-19 21:46:47 +0700

    Finish

--- imports/ui/parents/parents.js
+++ imports/ui/parents/parents.js
@@ -5,12 +5,54 @@ import './parents.html';
 
 var list;
 
+Template.parents.onCreated(function(){      
+  this.checkedParent = new ReactiveVar()
+  this.parent = null
+});
+
+Template.parent_param.helpers({
+  
+  child_selected(child, child_id){
+    return child == child_id ? 'selected' : ''
+  }
+})
+
+Template.parents.helpers({
+  checked(parent_id){
+    return Template.instance().checkedParent.get() == parent_id
+  },
+})
+
 
 Template.parents.events({
   'click .delete'() {   
     //console.log(this.elem._id);      
     Parents.remove(this.elem._id);
   },  
+  'click .one_parent'(event,instance){
+    event.preventDefault()
+    const target = event.target
+    if(instance.checkedParent.get() == target.id)
+      instance.checkedParent.set(null)
+    else
+      instance.checkedParent.set(target.id)
+    console.log(instance.checkedParent.get())
+  },
+  'submit .parent_info'(event) {
+    event.preventDefault();
+    const target = event.target;  
+    const name = target.name.value;
+    const surname = target.surname.value;
+    const patronymic = target.patronymic.value;
+    const address = target.address.value;
+    const child = target.child.value;
+    console.log(child)
+    Parents.update({_id:Template.instance().checkedParent.get()},{$set:{'name':name}})
+    Parents.update({_id:Template.instance().checkedParent.get()},{$set:{'surname':surname}})
+    Parents.update({_id:Template.instance().checkedParent.get()},{$set:{'patronymic':patronymic}})
+    Parents.update({_id:Template.instance().checkedParent.get()},{$set:{'address':address}})
+    Parents.update({_id:Template.instance().checkedParent.get()},{$set:{'child':child}})
+  },
   'submit .my_form'(event) {
     // Prevent default browser form submit
     event.preventDefault();
--- imports/ui/specializations/specializations.js
+++ imports/ui/specializations/specializations.js
@@ -4,19 +4,49 @@ import { Template } from 'meteor/templating';
 import { Subjects, Faculties, Specializations } from '../../api/database.js';
 import './specializations.html';
 
-var specializations_list;
 
-var faculties_list;
+Template.specializations.onCreated(function(){      
+  this.checkedSpec = new ReactiveVar()
+  
+});
 
+Template.spec_param.helpers({
+  
+  fac_selected(faculty, fac_id){
+    console.log(faculty)
+    console.log(fac_id)
+    return faculty == fac_id ? 'selected' : ''
+  }
+})
 
-
-
+Template.specializations.helpers({
+  checked(spec_id){
+    return Template.instance().checkedSpec.get() == spec_id
+  },
+})
 
 Template.specializations.events({
   'click .delete'() {   
     //console.log(this.elem._id);      
     Specializations.remove(this.elem._id);
-  },  
+  } ,  
+  'click .one_spec'(event,instance){
+    event.preventDefault()
+    const target = event.target
+    if(instance.checkedSpec.get() == target.id)
+      instance.checkedSpec.set(null)
+    else
+      instance.checkedSpec.set(target.id)
+    //console.log(instance.checkedParent.get())
+  },
+  'submit .spec_info'(event) {
+    event.preventDefault();
+    const target = event.target;  
+    const name = target.name.value;
+    const faculty = target.faculty.value;
+    Specializations.update({_id:Template.instance().checkedSpec.get()},{$set:{'name':name}})
+    Specializations.update({_id:Template.instance().checkedSpec.get()},{$set:{'faculty':faculty}})
+  },
   'submit .my_form'(event) {
     // Prevent default browser form submit
     event.preventDefault();
